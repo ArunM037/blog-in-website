584b2c2b0e98e795de0b05f9d83f77774785984e
Arun M <arunm2020@psnacet.edu.in>
2024-07-09 10:30:36 +0530

    fix the timeout

--- src/useFetch.js
+++ src/useFetch.js
@@ -8,28 +8,26 @@ const useFetch = (url) => {
 
     useEffect(() => {
         const abort = new AbortController();
-        setTimeout(() => {
-            fetch(url, { signal: abort.signal })
-                .then(res => {
-                    if (!res.ok) {
-                        throw Error('Failed to fetch');
-                    }
-                    return res.json();
-                })
-                .then(data => {
-                    setData(data);
+        fetch(url, { signal: abort.signal })
+            .then(res => {
+                if (!res.ok) {
+                    throw Error('Failed to fetch');
+                }
+                return res.json();
+            })
+            .then(data => {
+                setData(data);
+                setIsPending(false);
+                setError(null);
+            })
+            .catch(err => {
+                if (err.name === 'AbortError') {
+                    console.log('fetch aborted');
+                } else {
                     setIsPending(false);
-                    setError(null);
-                })
-                .catch(err => {
-                    if (err.name === 'AbortError') {
-                        console.log('fetch aborted');
-                    } else {
-                        setIsPending(false);
-                        setError(err.message);
-                    }
-                });
-        }, 1000);
+                    setError(err.message);
+                }
+            });
         return () => abort.abort();
     }, [url]);
 
